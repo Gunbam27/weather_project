b6c2e18a86715106b6fee5bee35a143e97962e8e
Yejin.Shin <mingmi_27@naver.com>
2024-04-18 21:55:28 +0900

    feat: enum code added

---
+++ lib/domain/model/weather_code_enum.dart
@@ -0,0 +1,10 @@
+// enum WeatherCode {
+//   // SUN(Icon(Icons.su)),
+//   // CLOUD('assets/images/cloud.png'),
+//   // RAIN('assets/images/rain.png'),
+//   // SNOW('assets/images/snow.png');
+//   //
+//   // final IconData icon;
+//   //
+//   // const WeatherCode(this.icon);
+// }
