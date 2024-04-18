8d9f882691b84dad0960bfd9261ec5e35497c083
sungchanjung <wjdtjdcks31@gmail.com>
2024-04-18 15:34:29 +0900

    feat: create api, mapper

---
+++ lib/data/data_source/hourly_api.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../dto/hourly_dto.dart';
+import '../dto/weather_dto.dart';
+
+void main() async {
+
+
+ final data = await WeatherDataSource().getWeather(52.52, 13.41);
+ print(data);
+}
+
+class WeatherDataSource {
+  Future<HourlyDto?> getWeather(double lat, double lng) async {
+    final response = await http.get(Uri.parse(
+        'https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lng&hourly=temperature_2m,rain,weather_code'));
+    return WeatherDto.fromJson(jsonDecode(response.body)).hourly;
+  }
+}
